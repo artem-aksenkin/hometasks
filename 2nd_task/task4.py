4585144aa5dd2a7b1c3ba75208f09aa999a6fb76
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-17 15:48:35 +0300

    report commit

--- 2nd_task/task4.py
+++ 2nd_task/task4.py
@@ -5,6 +5,6 @@ ipaddrs = []
 for line in open('access.log', 'r'):
     ipaddrs.append(line[:(line.index('-')-1)])
 
-ips_tuple_list = Counter(ipaddrs).most_common(10)
-for i in ips_tuple_list:
+ipaddrs_tuple = Counter(ipaddrs).most_common(10)
+for i in ipaddrs_tuple:
     print(i[0])
