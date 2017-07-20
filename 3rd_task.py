f83b41f88d5deb24c82942758c375d041142dcb0
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-20 10:15:25 +0300

    3rdTask  commit

--- 3rd_task.py
+++ 3rd_task.py
@@ -3,16 +3,29 @@ import datetime
 import configparser
 import json
 config = configparser.ConfigParser()
-config.read('/home/student/config')
-#a = config.get([('output', 'txt')])
-b = config.get('common', 'output')
+config.read('config')
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+config_name = ConfigSectionMap("common")['output']
+config_interval = ConfigSectionMap("common")['interval']
 now = datetime.datetime.now()
-if b == True:
-    with open('/home/student/log.txt', 'a+') as f:
-        num_str = str(f.tell())
-        f.write('SHAPSHOT {}: {}  '.format(num_str[:-2], now.strftime("%Y-%m-%d %H:%M:%S")))
-        f.write('CPU {}%  '.format(psutil.cpu_percent(interval=1)))
-        f.write('MemUsage {} MB  '.format(psutil.virtual_memory().used//1024//1024))
-        f.write('Swap {}  '.format(psutil.swap_memory().used/1024//1024))
-        f.write('Disk IO {} sec  '.format(psutil.disk_io_counters(perdisk=False).busy_time//100))
-        f.write('Net IO {}MB\n'.format(psutil.net_io_counters().bytes_sent//1024//1024))
+with open('/home/student/log.txt', 'a+') as f:
+    num_str = str(f.tell())
+    f.write('SHAPSHOT {}: {}  '.format(num_str[:-2], now.strftime("%Y-%m-%d %H:%M:%S")))
+    f.write('CPU {}%  '.format(psutil.cpu_percent(interval=1)))
+    f.write('MemUsage {} MB  '.format(psutil.virtual_memory().used//1024//1024))
+    f.write('Swap {}  '.format(psutil.swap_memory().used/1024//1024))
+    f.write('Disk IO {} sec  '.format(psutil.disk_io_counters(perdisk=False).busy_time//100))
+    f.write('Net IO {}MB\n'.format(psutil.net_io_counters().bytes_sent//1024//1024))
+mintime = int(config_interval) * 60
+time.sleep(mintime)
